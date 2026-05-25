ebc7b6881469d766bd6fe597b82246f91dbeeb7d
hanail1125 <hanail011250@gmail.com>
2026-05-25 12:16:05 +0900

    prefetch 재추가

--- components/movie.tsx
+++ components/movie.tsx
@@ -22,7 +22,7 @@ function Movie({title, id, poster_path}: IMovieProps) {
 				alt={title}
 				onClick={onClick}
 			/>
-			<Link href={`/movies/${id}`}>{title}</Link>
+			<Link prefetch href={`/movies/${id}`}>{title}</Link>
 		</div>
 	);
 }
