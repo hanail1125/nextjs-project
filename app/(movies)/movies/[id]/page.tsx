2d1e19302f45be86097ef7d9d08c6c15a09d8868
hanail1125 <hanail011250@gmail.com>
2026-05-25 13:02:36 +0900

    로딩중 스타일 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,33 +1,59 @@
-import React, { Suspense } from 'react';
-import MovieVideos from '../../../../components/movie-vodeos';
-import MovieInfo, { getMovie } from '../../../../components/movie-info';
+import React, { Suspense } from 'react'
+import MovieVideos from '../../../../components/movie-vodeos'
+import MovieInfo, { getMovie } from '../../../../components/movie-info'
 
 interface IParams {
-	params: { id: string };
+  params: { id: string }
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
-	const movie = await getMovie(id)
+export async function generateMetadata ({ params: { id } }: IParams) {
+  const movie = await getMovie(id)
 
-	return {
-		title: movie.title,
-	};
+  return {
+    title: movie.title
+  }
 }
 
-export default async function MovieDetailPage({
-	params: { id },
+export default async function MovieDetailPage ({
+  params: { id }
 }: {
-	params: { id: string };
+  params: { id: string }
 }) {
-	return (
-		<div>
-			<Suspense fallback={<h1>영화 정보 로딩중...</h1>}>
-				<MovieInfo id={id} />
-			</Suspense>
+  return (
+    <div>
+      <Suspense
+        fallback={
+          <h1
+            style={{
+              display: 'flex',
+              justifySelf: 'center',
+              alignItems: 'center',
+              height: '300px'
+            }}
+          >
+            영화 정보 로딩중...
+          </h1>
+        }
+      >
+        <MovieInfo id={id} />
+      </Suspense>
 
-			<Suspense fallback={<h1>비디오 정보 로딩중...</h1>}>
-				<MovieVideos id={id} />
-			</Suspense>
-		</div>
-	);
+      <Suspense
+        fallback={
+          <h1
+            style={{
+              display: 'flex',
+              justifySelf: 'center',
+              alignItems: 'center',
+              height: '300px'
+            }}
+          >
+            비디오 정보 로딩중...
+          </h1>
+        }
+      >
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  )
 }
